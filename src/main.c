e2b1deed47ff77119ca465244cfda3000f75122d
Breno Hildebrand <hello@brenohildebrand.com>
2023-10-17 18:43:00 -0300

    okk

--- src/main.c
+++ src/main.c
@@ -6,11 +6,36 @@
 /*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:53:55 by bhildebr          #+#    #+#             */
-/*   Updated: 2023/10/12 17:54:12 by bhildebr         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:17:48 by bhildebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+void	init()
 {
+	// parse args
+	// read map
+	// validate map
+	// create window and init mlx
+}
+
+void	loop()
+{
+	render(/* map */);
+}
+
+void	render(/* map */)
+{
+	map_scale(/* map */);
+	map_rotate(/* */); // rotate on z
+	map_rotate(/* */); // rotate on x
+	map_center();
+	map_draw();
+}
+
+int	main(int argc, char *argv[])
+{
+	init();
+	while (1)
+		loop();
 	return (0);
 }
