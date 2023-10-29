ed88408c6b455fbd5e1b8a0fe0a6eb53ab3704cc
Breno Hildebrand <hello@brenohildebrand.com>
2023-10-29 02:51:23 -0300

    ok

--- test.c
+++ test.c
@@ -6,12 +6,13 @@
 /*   By: bhildebr <bhildebr@student.42.sp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:52:59 by bhildebr          #+#    #+#             */
-/*   Updated: 2023/10/28 16:11:43 by bhildebr         ###   ########.fr       */
+/*   Updated: 2023/10/29 01:57:05 by bhildebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42-Kit/actions/actions.h"
 #include "42-Kit/types/rmanager.h"
+#include "42-Kit/types/file.h"
 
 #include <stdlib.h>
 
@@ -28,10 +29,16 @@ t_i32	main(void)
 	// insert_rmanager(&rmanager, (t_usize)name);
 	// destroy_rmanager(&rmanager);
 
-	print("[ TESTING PRINT ]");
-	rmanager = NULL; 
-	for (int i = 1; i < 10001; i++)
-		insert_rmanager(&rmanager, (t_usize)malloc(i));
-	destroy_rmanager(&rmanager);
-	return (0);	
+	// print("[ TESTING PRINT ]");
+	// rmanager = NULL; 
+	// for (int i = 1; i < 10001; i++)
+		// insert_rmanager(&rmanager, (t_usize)malloc(i));
+	// destroy_rmanager(&rmanager);
+	// return (0);	
+	
+	t_file	file;
+	
+	read_file(file, "ok.txt");
+	print(file->buffer->data);
+	return (0);
 }
