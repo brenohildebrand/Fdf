ec3779bd02824c3ad22518b5042bd5b08607272f
Breno Hildebrand <hello@brenohildebrand.com>
2023-11-10 00:14:42 -0300

    wow

--- src/parse_file.c
+++ src/parse_file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_file.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bhildebr <bhildebr@student.42.sp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 14:40:47 by bhildebr          #+#    #+#             */
-/*   Updated: 2023/11/08 15:46:42 by bhildebr         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:15:10 by bhildebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,26 +36,41 @@ static void	next_color_update(t_fdf fdf)
 	t_u8	current_character;
 	t_u8	next_character;
 	t_i32	color;
+	t_i32	count;
 
-	current_character = next_file();
-	next_character = next_file();
 	color = 0;
-	current_character = next_file();
-	while (current_character != ' ' && current_character != '\n' && current_character != '\0')
+	current_character = current_file();
+	if (current_character == ',')
 	{
-		next_character = next_file();
-		color <<= 8;
-		color &= hex_to_number(current_character) * 16 + hex_to_number(next_character) * 1;
 		current_character = next_file();
+		next_character = next_file();
+		count = 0;
+		current_character = next_file();
+		while (current_character != ' ' && current_character != '\n' && current_character != '\0')
+		{
+			next_character = next_file();
+			color <<= 8;
+			color |= (hex_to_number(current_character) * 16 + hex_to_number(next_character) * 1);
+			count++;
+			current_character = next_file();
+		}
+		color <<= 8;
+		color |= 0xFF;
 	}
-	color <<= 8;
-	color &= 0xFF;
+	else
+	{
+		color <<= 8;
+		color |= 0x00;
+		color <<= 8;
+		color |= 0x00;
+		color <<= 8;
+		color |= 0x00;
+		color <<= 8;
+		color |= 0xFF;
+	}
 	push_rgba_vector(fdf->map->color, (struct s_rgba){
-		.r = (color >> 24 & 0xFF),
-		.g = (color >> 16 & 0xFF),
-		.b = (color >> 8 & 0xFF),
-		.a = (color >> 0 & 0xFF)
-	});
+		.r = (color >> 24 & 0xFF), .g = (color >> 16 & 0xFF),
+		.b = (color >> 8 & 0xFF), .a = (color >> 0 & 0xFF)});
 }
 
 static void	next_number_update(
@@ -116,9 +131,7 @@ void	parse_file(t_fdf fdf)
 			if (current_character != ' ')
 			{
 				next_number_update(fdf, coords);
-				current_character = current_file();
-				if (current_character == ',')
-					next_color_update(fdf);
+				next_color_update(fdf);
 				current_character = current_file();
 			}
 			else
